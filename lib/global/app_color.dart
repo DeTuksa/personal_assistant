49bcc77ff63d77778c6190b9c7015ab7ab4cdb89
DeTuksa <39960305+DeTuksa@users.noreply.github.com>
2021-01-01 16:34:06 +0100

    first commit

---
+++ lib/global/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+const appWhite = Color(0xFFFFFFFF);
+const appSilver = Color(0xFFC5C5C9);
+const appPurple = Color(0xFF9F86FF);
+const appCyan = Color(0xFF00CFFF);
+const appBlue = Color(0xFF004DD1);
+const coolGray = Color(0xFF333845);
+const appBlack = Color(0xFF000000);
+const coolGray2 = Color(0xFF252830);
